6a137e370432d175ccc3fb9a32c28f9aebf28796
Alexander Wormbs <alexander.wormbs@consensys.net>
2019-07-12 09:37:43 +0100

    rename

--- src/utils/Web3Utils.ts
+++ src/utils/Web3Utils.ts
@@ -1,6 +1,8 @@
 import { CompiledContract } from "../types/CompiledContract"
-import { SolidityFile } from '../views/SolidityFiles'
 import Web3 from "web3"
+import { Event, Function, types } from "../types/ABITypes"
+import * as utils from 'web3-utils'
+
 const Web3Class = require('web3')
 var path = require('path')
 var fs = require('fs')
@@ -31,14 +33,12 @@ export function compileSolidity(filePath: string): CompiledContract[] | undefine
     }
 }
 
-export async function deployContract(contract: SolidityFile) {
-    const compiledContracts: CompiledContract[] | undefined = compileSolidity(contract.path)
-    if (compiledContracts) {
-        for (let contract of compiledContracts) {
-            const contractInstance = new web3.eth.Contract(contract.abi)
-            await contractInstance.deploy({ data: contract.bytecode, arguments: [] }).send({ from: accounts[0] })
-        }
-    }
+export async function deployContract(contract: CompiledContract): Promise<string> {
+    const contractInstance = new web3.eth.Contract(contract.abi)
+    contractInstance.deploy({ data: contract.bytecode, arguments: [] }).send({ from: accounts[0], gas: 1000000 })
+        .on('receipt', receipt => {
+            return receipt.contractAddress
+        })
 }
 
 export async function connectToBlockchain(blockchain_address: string) {
@@ -50,6 +50,39 @@ export function getCompiledFiles(): CompiledContract[] {
     return contractData
 }
 
+export function getEventData(abi: any[]): Event[] {
+    let events: Event[] = []
+    return getAbiData<Event>(abi, events, types.event)
+}
+
+export function getFunctionData(abi: any[]): Function[] {
+    let functions: Function[] = []
+    return getAbiData<Function>(abi, functions, types.function)
+}
+
+export function getAbiData<T>(abi: any[], types: T[], type: string): T[] {
+    for (let entity of abi) {
+        if (entity.type === type) {
+            types.push(entity)
+        }
+    }
+    return types
+}
+
+export function decodeEvent(event: Event, data: string) {
+    let eventTopic: string = web3.eth.abi.encodeEventSignature(event)
+    return web3.eth.abi.decodeLog(event.inputs, data, [eventTopic])
+}
+
+export async function sendTransaction(contractData: CompiledContract, contractAddress: string, functionName: string, args?: any[]) {
+    var contract = new web3.eth.Contract(contractData.abi, contractAddress, {
+        data: contractData.bytecode,
+        from: accounts[0],
+        gasPrice: '10',
+        gas: 100000
+    })
+    await contract.methods[functionName]().send({ from: accounts[0] })
+}
 
 function generateSolidityCompilerInput(filename: string, filecontent: string) {
     return {
