3ae08de3fbbcea9a1fdcc5bcdf7b72e0dce19608
Alexander Wormbs <alexander.wormbs@consensys.net>
2019-07-12 09:37:01 +0100

    added abi types

---
+++ src/types/ABITypes.ts
@@ -0,0 +1,37 @@
+export interface Event {
+    anonymous: boolean,
+    inputs: EventParameter[],
+    name: string,
+    type: 'event'
+}
+
+export interface Function {
+    constant: boolean,
+    inputs: Parameter[],
+    name: string,
+    outputs: Parameter[],
+    payable: boolean,
+    stateMutability: stateMutabilities,
+    type: 'function'
+}
+
+export enum types {
+    function = 'function',
+    event = 'event'
+}
+
+interface Parameter {
+    name: string,
+    type: string
+}
+
+interface EventParameter extends Parameter {
+    indexed: boolean
+}
+
+enum stateMutabilities {
+    view = 'view',
+    pure = 'pure',
+    nonpayable = 'nonpayable',
+    payable = 'payable'
+}
